4adcae128af076ddc69181d193846bf159830ec4
Tabish Ahmad <tabish0211@hotmail.com>
2023-09-08 22:37:43 +0530

    Add project files.

---
+++ Tabish_Console/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+namespace Tabish_Console
+{
+    class Program
+    {
+        //OS___CLR---Main
+        static void Main()
+        {
+            Console.WriteLine("telle me your name");
+            string name = Console.ReadLine();//input statement
+            Console.WriteLine("tell me your age");
+            int age =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello "+ name +" Your age is "+age);
+            Console.WriteLine($"Hello {name} Your age is {age}");
+            Console.WriteLine("Hello {0} your age is {1}", name,age);
+
+            Console.ReadLine();
+
+        }
+    }
+}
