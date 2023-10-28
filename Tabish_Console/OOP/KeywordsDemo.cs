8d2321eb80f43905361acecf325fcfb069b56bf2
Tabish Ahmad <tabish0211@hotmail.com>
2023-10-28 21:50:33 +0530

    kewords collections

---
+++ Tabish_Console/OOP/KeywordsDemo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabish_Console.OOP
+{
+    public class KeywordsDemo
+    {
+        public void Sum(ref int x, ref int y, out int z, out int m)
+        {
+            x = 3; y = 5;
+            z = x + y;
+            m = x - y;
+           
+        }
+
+
+        public static Tuple<string[],int,double> TupleDemo()
+        {
+            string[] str = { "A", "B", "C" };
+            string name = "Rakesh";
+            int salary = 10000;
+            double hra = 100.00;
+
+           return Tuple.Create(str, salary,hra);
+
+
+        }
+
+    }
+
+    class StudentsStrenghth
+    {
+        public readonly  int capacity ;
+
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+
+        public int Class { get; set; }
+
+        
+        public StudentsStrenghth(int cap)
+        {
+            capacity = cap;
+
+
+        }
+
+        public StudentsStrenghth()
+        {
+            
+
+        }
+
+
+
+
+
+    }
+
+}
--- Tabish_Console/OOP/Launcher.cs
+++ Tabish_Console/OOP/Launcher.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -108,16 +109,185 @@ namespace Tabish_Console.OOP
 
         //}
 
+        //public static void Main()
+        //{
+
+        //    I1 obj = new calculation();
+        //    obj.add();
+
+        //    I2 obj1 = new calculation();
+        //    obj.add();
+        //}
+
+        //public static void Main()
+        //{
+        //    int x = 1;
+        //    int y = 2;
+
+
+        //    KeywordsDemo demo = new KeywordsDemo();
+
+        //    demo.Sum(ref x, ref y, out int z, out int m);
+        //    Console.WriteLine($"{z} {m}");
+        //    Console.WriteLine($"{x} {y}");
+        //    Console.ReadLine();
+
+        //}
+
+
         public static void Main()
         {
+            // Console.WriteLine($"Enter the capacity for the {DateTime.Now.Year}");
+            // int capcity = Convert.ToInt32(Console.ReadLine());
 
-            I1 obj = new calculation();
-            obj.add();
+            // StudentsStrenghth obj = new StudentsStrenghth(capcity);
 
-            I2 obj1 = new calculation();
-            obj.add();
+            // Console.WriteLine(obj.capacity);
+            //// obj.capacity = 300;
+            //// StudentsStrenghth.capacity = 200;
+            // //Console.WriteLine(StudentsStrenghth.capacity);
+
+            //string str = "Rakesh";//string immutable 
+            //str = str + " " + "Kumar";
+
+            //string data = string.Empty;
+            //for (int i = 0; i < length; i++)
+            //{
+            //    data += data + " ";//new string data object---bulky
+            //}
+
+
+            //StringBuilder str = new StringBuilder();//Mutable
+            //str.Append("Rakesh");
+            //str.Append(" ");
+            //str.Append("Kumar");
+
+            //Console.WriteLine(str.ToString()); ;
+
+            //StudentsStrenghth students = new StudentsStrenghth(10);
+            //students.Name = "Rakesh";
+            //students.RollNo = 1001;
+            //students.Class = 10;
+
+            ////object initialiser
+            //var student = new StudentsStrenghth(10)
+            //{
+
+            //    Name = "Rakesh",
+            //    RollNo = 1001,
+            //    Class = 10
+            //};
+
+            //var myrecords=KeywordsDemo.TupleDemo();
+            //Console.WriteLine($"{myrecords.Item1[0]}  {myrecords.Item2} {myrecords.Item3}");
+
+            //Console.WriteLine("enetr the size");
+            //int size = Convert.ToInt32(Console.ReadLine());
+            //StudentsStrenghth[] arrStudents=new StudentsStrenghth[size];
+
+            //List<StudentsStrenghth> students = new List<StudentsStrenghth>();
+            //StudentsStrenghth s1 = new StudentsStrenghth{ Name = "Maya", Class = 1, RollNo = 2001 };
+            //StudentsStrenghth s2 = new StudentsStrenghth { Name = "Maya", Class = 1, RollNo = 2001 };
+
+            //students.Add(s1);
+            //students.Add(s2);
+
+            //var students = new List<StudentsStrenghth>
+
+            //{
+            //new StudentsStrenghth{ Name = "Maya", Class = 1, RollNo = 2001 },
+            //new StudentsStrenghth { Name = "Maya", Class = 1, RollNo = 2001 }
+
+
+            //};
+
+
+            //foreach (var item in students)
+            //{
+            //    Console.WriteLine($"{item.Name} {item.RollNo} {item.Class}");
+            //}
+
+            //ArrayList arrList=new ArrayList {
+            //new StudentsStrenghth{ Name = "Maya", Class = 1, RollNo = 2001 },
+            //new StudentsStrenghth { Name = "Maya", Class = 1, RollNo = 2001 },
+            //"Rahul"
+
+
+            //};
+
+            //foreach (var item in arrList)
+            //{
+            //    if (item.GetType().Equals(typeof(StudentsStrenghth)))
+            //    {
+            //        var student = (StudentsStrenghth)item;
+            //        Console.WriteLine($"{student.Name} {student.RollNo} {student.Class}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+
+            //}
+
+            //            Console.WriteLine(arrStudents.Length);
+
+
+            //int Maharashtra = 12000;
+            //int TamilNadu = 30000;
+            //int Delhi = 50000;
+
+            //int Dubai = 80000;
+            //int abudhabi = 20000;
+
+            //int IndiaPopulation = Maharashtra + TamilNadu + Delhi;
+            //int UAEPopulation = Dubai + abudhabi;
+
+            //Dictionary<string, int> countryPopulation = new Dictionary<string, int>();
+            //countryPopulation.Add("India", IndiaPopulation);
+            //countryPopulation.Add("uaePopulation", UAEPopulation);
+
+            ////from yopu said
+
+            //countryPopulation.TryGetValue("India", out int populationofIndia);
+            //countryPopulation.TryGetValue("uaePopulation", out int populationofUAE);
+
+            //Console.WriteLine($"Population of India ={populationofIndia} and population of UAE={populationofUAE}");
+
+            //List<string> countries = new List<string>() {
+            //"India","Singapore"
+
+            //};
+
+
+
+            List<int> lstOne = new List<int> { 1, 2, 3 };
+            List<int> lsttwo = new List<int> { 1, 2, 3 };
+
+            //List<List<int>> lstodLst = new List<List<int>> {
+            //lstOne,
+            //lsttwo
+            //};
+
+            List<List<int>> lstodLst = new List<List<int>>();
+            lstodLst.Add(lstOne);
+            lstodLst.Add(lsttwo);
+
+
+            foreach (var lst in lstodLst)
+            {
+                foreach (var item in lst)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+
+
+
+            Console.ReadLine();
+
         }
-}
+    }
 
 
 }
